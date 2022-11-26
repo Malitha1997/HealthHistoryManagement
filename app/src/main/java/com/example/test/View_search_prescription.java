a199259222614d6ed2c54d646092214a7c7785df
Malitha1997 <halahakoon10480@usci.ruh.ac.lk>
2022-11-26 22:20:59 +0530

    UI created

---
+++ app/src/main/java/com/example/test/View_search_prescription.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class View_search_prescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_search_prescription);
+    }
+}
