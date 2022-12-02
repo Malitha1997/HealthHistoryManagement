74eb70ffd71e2005186e3e95467984a93f855767
Malitha1997 <halahakoon10480@usci.ruh.ac.lk>
2022-12-02 09:59:32 +0530

    prescription view added

--- app/src/main/java/com/example/test/My_health_booklet.java
+++ app/src/main/java/com/example/test/My_health_booklet.java
@@ -2,17 +2,40 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class My_health_booklet extends AppCompatActivity {
+    TextView lifeCareBooklet;
+    TextView prescriptionView;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_health_booklet);
+
+        lifeCareBooklet=findViewById(R.id.lifeCareBooklet);
+        prescriptionView=findViewById(R.id.prescriptionView);
+
+        lifeCareBooklet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Home.class);
+                startActivity(i);
+            }
+        });
+
+        prescriptionView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),View_search_prescription.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
--- app/src/main/java/com/example/test/Side_effects.java
+++ app/src/main/java/com/example/test/Side_effects.java
@@ -2,13 +2,27 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Side_effects extends AppCompatActivity {
+    TextView lifeCareSideeffects;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_side_effects);
+
+        lifeCareSideeffects=findViewById(R.id.lifeCareSideeffects);
+
+        lifeCareSideeffects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(),Home.class);
+                startActivity(i);
+            }
+        });
     }
 }
